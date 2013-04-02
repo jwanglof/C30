18678a2e46953e0b0789870b50c2a9d639e98519
Johan Wanglof <jwanglof@gmail.com>
2013-04-02 20:04:31 +0200

    jadaaddad

---
+++ sendform.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$school_class = $_POST['school_class'];
+$allergies = $_POST['allergies'];
+
+// Send the form to the correct c30-mail
+/*
+if (success) {
+	
+}
+*/
+
+?>
